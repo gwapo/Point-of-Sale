d66f77d3a0b052af3799228c7d7a712c5c51e31e
Dennis Carique <denniscarique@gmail.com>
2011-07-08 16:04:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypos::Application.config.secret_token = '3b31751e6e1e2b26a53b86120031e05251a71eeb4f17fb8354714e41de7882f870a2cd5c77f13c7b5feaf0c539e05543830f1ffbffd393ad45bdeb51ca1450e4'
